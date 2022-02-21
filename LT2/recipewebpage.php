42a6311d71205a5601941c60c339dbfc9eb3dbd0
OzzyLedesma <100151495+OzzyLedesma@users.noreply.github.com>
2022-02-22 01:49:23 +0800

    Create recipewebpage.php

---
+++ LT2/recipewebpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+<title>My Online Recipe Website</title>
+</head>
+<body>
+<h1>Recipes</h1>
+<ul>
+<li><a href="chilicorn.html">Chilli con Carne</a></li>
+<li><a href="beefnbeer.html">Beef in Beer</a></li>
+<li><a href="padthai.html">Pad Thai Chicken</a></li>
+<li><a href="grencury.html">Thai Green Curry</a></li>
+<li><a href="pinaplechick.html">Pineapple chicken</a></li>
+<li><a href="vegrice.html">Vegetarian rice</a></li>
+<li><a href="ommlet.html">Omelette</a></li>
+</ul>
+</body>
+</html>
